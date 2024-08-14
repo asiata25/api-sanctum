bd278b0078ab7eb83c3dd9146198482d870cf200
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-08-14 18:47:04 +0700

    feat: crud post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => ['required', 'max:255'],
+            'description' => ['required']
+        ]);
+
+        $post = Post::create($validated);
+
+        return response()->json($post)->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        $validated = $request->validate([
+            'title' => ['required', 'max:255'],
+            'description' => ['required']
+        ]);
+
+        $post->update($validated);
+
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json(['message' => 'ok']);
+    }
+}
